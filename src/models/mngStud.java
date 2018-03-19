e9c1230f627049cbeeae9ce0b4720874b0a9d03d
Aditya Chadalwada <adityac564@gmail.com>
2018-03-19 18:46:46 +0530

    java files
    company , alumni , students , TPC members

---
+++ src/models/mngStud.java
@@ -0,0 +1,21 @@
+package models;
+
+import java.util.ArrayList;
+
+public class mngStud extends user{
+	private ArrayList<String> skill=new ArrayList<String>();
+	private ArrayList<String> intern=new ArrayList<String>();
+	
+	public void setSkill(String skill){
+		this.skill.add(skill);
+	}
+	public ArrayList<String> getSkill(){
+		return skill;
+	}
+	public void setIntern(String intern){
+		this.intern.add(intern);
+	}
+	public ArrayList<String> getIntern(){
+		return intern;
+	}
+}
